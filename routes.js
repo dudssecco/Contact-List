82ea0e7018969ec0d26f531bfc00e2e33cadd63f
Eduardo Secco <dudssecco.code@gmail.com>
2024-05-22 00:51:47 -0300

    Update

--- routes.js
+++ routes.js
@@ -2,15 +2,17 @@ const express = require('express');
 const route = express.Router();
 const HomeController = require('./src/controllers/HomeController');
 const LoginController = require('./src/controllers/LoginController');
-const RegisterController = require('./src/controllers/RegisterController')
+const RegisterController = require('./src/controllers/RegisterController');
 
 // Rotas da Home
 route.get('/', HomeController.index);
 
 // Rotas de Login
 route.get('/login/index', LoginController.index)
+route.post('/login', LoginController.index)
 
-// Rotas de Register
+// Rotas de Registro
 route.get('/register/index', RegisterController.index)
+route.post('/register', RegisterController.register)
 
 module.exports = route;
--- src/controllers/RegisterController.js
+++ src/controllers/RegisterController.js
@@ -1,3 +1,31 @@
+const Register = require('../models/RegisterModel')
+
 exports.index = (req, res) => {
     res.render('register')
+}
+
+exports.register = async (req, res) => {
+    try{
+        const registro = new Register(req.body)
+        await registro.createUser()
+
+        if(registro.errors.length > 0){
+            req.flash('errors', registro.errors)
+            req.session.save(function() {
+                return res.redirect('/register/index')
+        })
+        return
+    }
+
+    req.flash('success', 'Usuário criado com sucesso.')
+    req.session.save(function() {
+        return res.redirect('/register/index') 
+    })
+
+    } catch(e) {
+        console.log(e)
+        return res.render('error404')
+    }
+
+    
 }
